b2588f923f6077420049f547abcf0de747a907ce
Brian Bui <iambrianbui@yahoo.com>
2018-09-07 11:47:15 -0700

    Added skyintruders folder and skyintruders.py

---
+++ skyintruders/skyintruders.py
@@ -0,0 +1,28 @@
+import sys
+
+import pygame
+
+def run_game():
+    #  Initialize game and create a screen object.
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Sky Intruders")
+
+    #  Set the bg color
+    bg_color = (115, 196, 252)
+
+    #  Start the main loop for the game.
+    while True:
+
+        #  What for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        #  Redraw the screen during each pass through the loop/
+        screen.fill(bg_color)
+
+        #  Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+run_game()
